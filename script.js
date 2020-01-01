8f5c0ca9d3b3177acf1dd42a21ceb496582ed0ff
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 22:32:50 +0900

    タイトルクリック時　背景色変更

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ $(function() {
   });
   $('.expense-title').hover(function(){
     $('.expense-hide').css('display','block')
+    $(this).css('background-color','fuchsia');
   });
+
+  $('#title').hover(function(){
+    $(this).css('background-color','deepskyblue');
+  });
+
+
 });
