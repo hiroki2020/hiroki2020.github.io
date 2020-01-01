840f505ba700bfb36961f86fbe49b9eae4e4b534
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 22:03:17 +0900

    jquery

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.meru-title').hover(function(){
+    $('.meru-hide').css('display','block')
+  });
+  $('.expense-title').hover(function(){
+    $('.expense-hide').css('display','block')
+  });
+});
