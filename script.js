627b200153f35bdf4632dc8dadc6a9102069fa06
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-14 21:36:00 +0900

    animation when click header menus
    ヘッダーメニューをクリックした時の効果

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 $(function() {
-  $('.meru-title').hover(function(){
-    $('.meru-hide').css('display','block')
+  $('#about-query').click(function(){
+    $('#about-query a').css('color','dimgray');
+    $('#works-query a, #servise-query a, #contact-query a').css('color','black');
   });
-  $('.chat-title').hover(function(){
-    $('.chat-hide').css('display','block')
-  });
-  $('.expense-title').hover(function(){
-    $('.expense-hide').css('display','block')
-    $(this).css('background-color','violet');
-  });
-  $('').hover(function(){
-    $().css('background-color','deepskyblue');
-  });
-  $('.chat-title').hover(function(){
-    $(this).css('background-color','darkturquoise');
+  
+
+  $('#works-query').click(function(){
+    $('#works-query a').css('color','dimgray');
+    $('#about-query a, #servise-query a, #contact-query a').css('color','black');
   });
 
+  $('#servise-query').click(function(){
+    $('#servise-query a').css('color','dimgray');
+    $('#about-query a, #works-query a, #contact-query a').css('color','black');
+  });
+
+  $('#contact-query').click(function(){
+    $('#contact-query a').css('color','dimgray');
+    $('#about-query a, #works-query a, #servise-query a').css('color','black');
+  });
+  
 });
