c8524651ccbb826acf516c89159a5d6a5741ff21
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-28 22:06:22 +0900

    最新版
    the latest

--- scroll-arrow.js
+++ scroll-arrow.js
@@ -1,5 +1,48 @@
 $(function(){
+
   $('.scroll-arrow').on("click", function(){
     $('html').animate({scrollTop: 0});
   });
+
+  $('#about').on("click", function(){
+    $('html').animate({scrollTop: 0})
+  });
+
+  $('#skill').on("click", function(){
+    $('html').animate({scrollTop: 800})
+  });
+
+  $('#skill-1').on("click", function(){
+    $('html').animate({scrollTop: 950})
+  });
+
+  $('#skill-2').on("click", function(){
+    $('html').animate({scrollTop: 1680})
+  });
+
+  $('#skill-3').on("click", function(){
+    $('html').animate({scrollTop: 2600})
+  });
+
+  $('#skill-4').on("click", function(){
+    $('html').animate({scrollTop: 3450})
+  });
+
+  $('#works').on("click", function(){
+    $('html').animate({scrollTop: 4310})
+  });
+
+  $('#work-1').on("click", function(){
+    $('html').animate({scrollTop: 4430})
+  });
+
+
+  $('#work-2').on("click", function(){
+    $('html').animate({scrollTop: 5250})
+  });
+
+  $('#contact').on("click", function(){
+    $('html').animate({scrollTop: 5800})
+  });
+
 });
